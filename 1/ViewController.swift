07b23d1be58fa5d28af480222a3c68dfa60cd742
KEKSman <bassman7009@gmail.com>
2020-04-06 16:40:08 +0500

    Initial Commit

---
+++ 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Евгений колесников on 06.04.2020.
+//  Copyright © 2020 Евгений колесников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
